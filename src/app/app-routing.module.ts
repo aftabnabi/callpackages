0c9d1f920708f512c107f12370bd6209467a3bf2
Aftab Nabi <aftab.pucit@gmail.com>
2021-07-03 20:08:20 +0500

    created frontpage components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontpageComponent } from './components/pages/frontpage/frontpage.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: '', component: FrontpageComponent},
+  {path: 'frontpage', component: FrontpageComponent}
+  
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule} from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/pages/frontpage/header/header.component';
+import { AdComponent } from './components/pages/frontpage/ad/ad.component';
+import { SeperatorComponent } from './components/pages/frontpage/seperator/seperator.component';
+import { CreditcardsComponent } from './components/pages/frontpage/creditcards/creditcards.component';
+import { FooterComponent } from './components/pages/frontpage/footer/footer.component';
+import { FrontpageComponent } from './components/pages/frontpage/frontpage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AdComponent,
+    SeperatorComponent,
+    CreditcardsComponent,
+    FooterComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule,
